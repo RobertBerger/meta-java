e2be05d279c229d6f5aab434f657713bb0e08358
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-05-09 12:53:26 +0300

    added send-patch sample

--- my-scripts/doc/send-patch.sh
+++ my-scripts/doc/send-patch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-git send-email --from robert.berger@ReliableEmbeddedSystems.com --cc richard.leitner@skidata.com --to openembedded-devel@lists.openembedded.org $1
+git send-email --from robert.berger@ReliableEmbeddedSystems.com --to robert.karl.berger@gmail.com $1
+#git send-email --from robert.berger@ReliableEmbeddedSystems.com --cc richard.leitner@skidata.com --to openembedded-devel@lists.openembedded.org $1
 set +x
